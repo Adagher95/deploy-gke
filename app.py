ceb79fc287d3f931f736d6a16950a846c57dbcc2
Adagher <Adagher@liverx.me>
2024-10-19 12:29:24 +0000

    update info

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hello! I'm Ahmad Dagher. Google Cloud Architect!</h1>"
+    return "<h1>Hello! My name is Ahmad Dagher. Google Cloud Architect !</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
