d679f0b8f0f97980ce63c9d4229b2b9ca36cb429
Ahmad Dagher <161319327+Adagher95@users.noreply.github.com>
2024-10-24 16:00:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hello! My name is  Ahmad Dagher.  Google Cloud Architect!</h1>"
+    return "<h1>Hello! My name is  Ahmad Dagher.  Google Cloud Architect As a Professional service engineer, I am tasked with designing and
+implementing strong, flexible systems that will adapt to the different needs of
+businesses. In particular, I have concentrated on Google Cloud Architecture
+and helped companies migrate by optimizing the platform using Google
+Cloud.</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
